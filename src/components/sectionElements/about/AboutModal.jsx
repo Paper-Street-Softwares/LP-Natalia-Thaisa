71a5d588f50c82755da507d7299671fec8dd8273
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-15 16:10:23 -0300

    fix: Reviso final

--- src/components/sectionElements/about/AboutModal.jsx
+++ src/components/sectionElements/about/AboutModal.jsx
@@ -15,14 +15,10 @@ export default function AboutModal() {
 
     setModalContent(
       <div className="text-paragraph3">
-        {/* <p className="mb-[20px]">
-          <Trans i18nKey="about.subtitle" />
-        </p> */}
-
         <Trans i18nKey="about.modal" components={{ br: <br /> }} />
 
         <div className="mt-4">
-          <p className="mt-[25px] mb-[20px]">
+          <p className=" mb-[20px]">
             <Trans i18nKey="about.ctaButtonAriaLabel" />
           </p>
           <Button
